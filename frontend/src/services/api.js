a7a2cb5e654932944ad413fdf35e8cb5db1986d2
Gláucio <glauciofilho@live.com>
2026-01-16 00:01:05 -0300

    updating an api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -43,7 +43,7 @@ export async function getAnalyticsProjects() {
 }
 
 export async function sendContactMessage(data) {
-  const res = await fetch(`${API_BASE}/contacts/`, {
+  const res = await fetch(`${API_BASE}/contact/form/`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
